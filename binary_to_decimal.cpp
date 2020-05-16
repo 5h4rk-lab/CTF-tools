e4962bcb9490720818c559a654866b2ffd6a5eae
5h4rk-lab <64501067+5h4rk-lab@users.noreply.github.com>
2020-05-16 10:19:24 -0700

    Update binary_to_decimal.cpp

--- binary_to_decimal.cpp
+++ binary_to_decimal.cpp
@@ -2,16 +2,11 @@
 // C++ program to convert binary to decimal 
 #include <iostream> 
 using namespace std; 
-  
-// Function to convert binary to decimal 
 int binaryToDecimal(int n) 
 { 
     int num = n; 
     int dec_value = 0; 
-  
-    // Initializing base value to 1, i.e 2^0 
-    int base = 1; 
-  
+    int base = 1;
     int temp = num; 
     while (temp) { 
         int last_digit = temp % 10; 
